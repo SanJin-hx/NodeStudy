73f43b4ecb47664132d7b8ea0dd4a2bd8a16a76b
SanJin-hx <823363796@qq.com>
2017-07-17 22:05:46 +0800

    JSprojrct

---
+++ JavaScriptProject/CatSay.js
@@ -0,0 +1,35 @@
+'use strict';
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Cat extends Animal{
+  constructor(name){
+    super(name);
+  }
+  say(){
+    return('Hello, '+this.name+'!');
+  }
+}
+var kitty = new Cat('Kitty');
+var doraemon = new Cat('哆啦A梦');
+if (kitty && kitty.name === 'Kitty' && kitty.say && typeof kitty.say === 'function' && kitty.say() === 'Hello, Kitty!' && kitty.say === doraemon.say) {
+    console.log('测试通过!');
+} else {
+    console.log('shibai');
+}
+/*function Cat(name){
+  this.name = name;
+}
+Cat.prototype.say = function(){
+  return('Hello, '+this.name+'!');
+};
+
+var kitty = new Cat('Kitty');
+var doraemon = new Cat('哆啦A梦');
+if (kitty && kitty.name === 'Kitty' && kitty.say && typeof kitty.say === 'function' && kitty.say() === 'Hello, Kitty!' && kitty.say === doraemon.say) {
+    console.log('测试通过!');
+} else {
+    console.log('shibai');
+}*/
